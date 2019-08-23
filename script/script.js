070c15de793ab9ac3d450e294e7200f19cbfe1a2
Nata <natalikrasnobaieva@gmail.com>
2019-08-23 16:34:51 +0300

    done

--- script/script.js
+++ script/script.js
@@ -5,6 +5,25 @@ let money,
     mission,
     period;
 
-alert("It's working!");
+money = 999;
+income = '99';
+addExpenses = 'Учеба, Поездки, Одежда';
+deposit = true;
+mission = '55555';
+period = 7;
 
-console.log("This is console!");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('Период' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'долларов');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+console.log(budgetDay%30);
+
